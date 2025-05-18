f399801caea9dadaa222b7b95eb3bdb2f58dcd5b
Artem Sikhvart <sixvart05@bk.ru>
2025-05-18 15:56:22 +0600

    Add filter is_favorited for RecipeViewSet

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,10 +5,11 @@ from .models import Recipe
 class RecipeFilter(filters.FilterSet):
     is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
     author = filters.NumberFilter(field_name='author__id')
+    is_favorited = filters.NumberFilter(method='filter_is_favorited')
 
     class Meta:
         model = Recipe
-        fields = ('author', 'is_in_shopping_cart')
+        fields = ('author', 'is_in_shopping_cart', 'is_favorited')
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
@@ -17,3 +18,11 @@ class RecipeFilter(filters.FilterSet):
         if value:
             return queryset.filter(in_carts__user=user)
         return queryset.exclude(in_carts__user=user)
+
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if not user.is_authenticated:
+            return queryset.none() if value else queryset
+        if value:
+            return queryset.filter(favorited_by__user=user)
+        return queryset.exclude(favorited_by__user=user)
